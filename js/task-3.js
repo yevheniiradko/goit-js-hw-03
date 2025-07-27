a032ffeb4278be6a8e97ce04464761475e97bffa
Yevhenii Radko <yevhenii.radko@gmail.com>
2025-07-27 12:16:22 +0300

    first commit

--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,16 @@
-function checkForSpam(message) {
-  const lowerCaseMessage = message.toLowerCase();
+function filterArray(numbers, value) {
+  const sumArray = [];
 
-  const hasSpam1 = lowerCaseMessage.includes('spam');
-  const hasSpam2 = lowerCaseMessage.includes('sale');
-
-  return hasSpam1 || hasSpam2;
+  for (const item of numbers) {
+    if (item > value) {
+      sumArray.push(item);
+    }
+  }
+  return sumArray;
 }
-console.log(checkForSpam('Latest technology news')); // false
-console.log(checkForSpam('JavaScript weekly newsletter')); // false
-console.log(checkForSpam('Get best sale offers now!')); // true
-console.log(checkForSpam('Amazing SalE, only tonight!')); // true
-console.log(checkForSpam('Trust me, this is not a spam message')); // true
-console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
-console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
